7f984da6fa22c6bc7b0e38f5ac51014b98b4cd3a
lucasmez <lucasmezalira@hotmail.com>
2017-01-12 17:04:57 -0500

    Rename interestSchema to InterestSchema

--- app_api/models/interest.js
+++ app_api/models/interest.js
@@ -1,10 +1,11 @@
 const 	mongoose = require('mongoose'),
         Schema = mongoose.Schema;
 
-const interestSchema = new mongoose.Schema({
+const InterestSchema = new mongoose.Schema({
 	title: {type: String, required:true},
 	description: String,
 	usersInterested: [Schema.Types.ObjectId]	
 });
 
-mongoose.model('Interest', interestSchema);
+
+mongoose.model('Interest', InterestSchema);
