a7d3be324803c170e6a48e9647c2408e39d58a59
lucasmez <lucasmezalira@hotmail.com>
2017-01-12 17:03:12 -0500

    Add password field to User model. Add authentication hook. Rename userSchema to UserSchema.

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -1,11 +1,39 @@
+const {DB_SALT_FACTOR} = require('../../config');
+const bcrypt = require('bcrypt');
+
 const mongoose = require('mongoose'),
       Schema = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
 	name: {type: String, required: true},
+    password: {type: String, required: true},
 	bio: String,
 	createdOn: {type: Date, "default": Date.now},
 	interests: [Schema.Types.ObjectId]
 });
 
-mongoose.model('User', userSchema);
+UserSchema.pre('save', function(next) {
+    let user = this;
+
+    if(!user.isModified("password"))
+        return next();
+    
+    bcrypt.genSalt(DB_SALT_FACTOR, (err, salt) => {
+        if(err)
+            return next(err);
+        
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if(err)
+                return next(err);
+            
+            user.password = hash;
+            next();
+        }); 
+    });
+});
+
+UserSchema.methods.authenticate = function(inputPass, cb) {
+    bcrypt.compare(inputPass, this.password, cb);
+};
+
+mongoose.model('User', UserSchema);
