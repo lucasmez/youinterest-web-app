486365b172df9bb774ff9bd87cb5f9afe24ccf25
lucasmez <lucasmezalira@hotmail.com>
2017-01-12 17:17:10 -0500

    Create index.js to export all controllers

---
+++ app_server/controllers/index.js
@@ -0,0 +1,4 @@
+module.exports.interestCtrl = require('./interestCtrl');
+module.exports.userCtrl = require('./userCtrl');
+module.exports.errorHandler = require('./errorHandler');
+module.exports.loginCtrl = require('./loginCtrl');
