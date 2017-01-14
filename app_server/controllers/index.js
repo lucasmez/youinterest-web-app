e5fe70bd0b4df1ad27ef2e9a5cdffaae5dc19778
lucasmez <lucasmezalira@hotmail.com>
2017-01-14 01:39:14 -0500

    Add /account route. Remove /interest/interestTitle route, now handled with React

--- app_server/controllers/index.js
+++ app_server/controllers/index.js
@@ -2,3 +2,4 @@ module.exports.interestCtrl = require('./interestCtrl');
 module.exports.userCtrl = require('./userCtrl');
 module.exports.errorHandler = require('./errorHandler');
 module.exports.loginCtrl = require('./loginCtrl');
+module.exports.accountCtrl = require('./accountCtrl');
