08efcb2ee40ea0e9a40f1b9509f76cbc629bd162
lucasmez <lucasmezalira@hotmail.com>
2017-01-14 01:38:14 -0500

    Add /account route and remove /interest/:interestTitle route, now handled with React

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,15 +1,16 @@
 const Router = require('express').Router;
-const {interestCtrl, userCtrl, errorHandler, loginCtrl} = require('../controllers');
+const {interestCtrl, userCtrl, errorHandler, loginCtrl, accountCtrl} = require('../controllers');
 
 const app = new Router();
 module.exports = app;
 
 
-app.get("/interest/:interestTitle", interestCtrl);
+//app.get("/interest/:interestTitle", interestCtrl);
 app.get("/user/:userName", userCtrl);
 app.get("/login", loginCtrl.loginRender);
 app.get("/logout", loginCtrl.doLogout);
 app.post("/login", loginCtrl.doLogin);
 
+app.get("/account", accountCtrl); // Return account info. if user is logged in. Used in the SPA
 
 app.use(errorHandler);
