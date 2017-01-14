f65e327e8fdb410e6687df2bd996bcf7a1dd5e3c
lucasmez <lucasmezalira@hotmail.com>
2017-01-14 16:21:16 -0500

    Create SPA app configuration file

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    yt_videos_max: 10,  // Max numbers of youtube videos to show on VideoList component
+    api_keys: {
+        youtube: "AIzaSyCMhzgR_M9WDdQti-WrqXBPn63N5QvdX5g"
+    }
+};
