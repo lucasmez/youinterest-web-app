991135eb194050ea3e5031db55bbdac8af10d94b
lucasmez <lucasmezalira@hotmail.com>
2017-01-16 22:26:02 -0500

    Change node.ENV to correct process.env

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,6 @@ module.exports = {
     yt_videos_max: 10,  // Max numbers of youtube videos to show on VideoList component
     search_delay: 500,  // Search bar delay in milliseconds
     api_keys: {
-        youtube: node.ENV.YOUTUBE_KEY || "Enter dev key"
+        youtube: process.env.YOUTUBE_KEY || "Enter dev key"
     }
 };
