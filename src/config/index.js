75029aa8b0b240ce4d2cfc759261a080af623b59
lucasmez <lucasmezalira@hotmail.com>
2017-01-16 21:52:19 -0500

    Change config to get youtube key from enviroment variable

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,6 @@ module.exports = {
     yt_videos_max: 10,  // Max numbers of youtube videos to show on VideoList component
     search_delay: 500,  // Search bar delay in milliseconds
     api_keys: {
-        youtube: "AIzaSyCMhzgR_M9WDdQti-WrqXBPn63N5QvdX5g"
+        youtube: node.ENV.YOUTUBE_KEY || "Enter dev key"
     }
 };
