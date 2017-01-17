e757269be6b1bf5df26315023431c4ac084f262f
lucasmez <lucasmezalira@hotmail.com>
2017-01-16 20:52:50 -0500

    Add search_delay configuration option in config file

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     yt_videos_max: 10,  // Max numbers of youtube videos to show on VideoList component
+    search_delay: 500,  // Search bar delay in milliseconds
     api_keys: {
         youtube: "AIzaSyCMhzgR_M9WDdQti-WrqXBPn63N5QvdX5g"
     }
